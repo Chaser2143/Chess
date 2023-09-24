ebe42a6cae49294a2fb694abb351e2e9e22bcc49
Chase Bledsoe <cb986@byu.edu>
2023-09-23 22:22:30 -0600

    Started beginning implementation

--- src/starter/chess/CBoard.java
+++ src/starter/chess/CBoard.java
@@ -2,18 +2,31 @@ package chess;
 
 public class CBoard implements ChessBoard{
 
+    private ChessPiece[][] board;
+    private int countingOffset = -1;
+
+    public CBoard(){
+        board = new ChessPiece[7][7]; //2D array anyone? Counting starts at ZERO
+    }
+
     @Override
     public void addPiece(ChessPosition position, ChessPiece piece) {
-
+        //Sets a specific position to a chess piece
+        int row = position.getRow() + countingOffset; //Positions start at 1, kinda gross
+        int col = position.getColumn() + countingOffset;
+        board[row][col] = piece;
     }
 
     @Override
     public ChessPiece getPiece(ChessPosition position) {
-        return null;
+        //Returns the chess piece at a position, else null
+        int row = position.getRow() + countingOffset; //Positions start at 1, kinda gross
+        int col = position.getColumn() + countingOffset;
+        return board[row][col];
     }
 
     @Override
     public void resetBoard() {
-
+        //TODO
     }
 }
--- src/starter/chess/CPosition.java
+++ src/starter/chess/CPosition.java
@@ -1,6 +1,24 @@
 package chess;
 
 public class CPosition implements ChessPosition{
+
+    int row;
+    int column;
+
+    public CPosition(int row, int column){
+        //Constructor, i think this is right... right?
+        setRow(row);
+        setColumn(column);
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
     @Override
     public int getRow() {
         return 0;
