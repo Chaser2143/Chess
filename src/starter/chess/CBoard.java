7d7b62078e52217e5eeab22d6cd48d964280df24
Chase Bledsoe <cb986@byu.edu>
2023-09-23 19:59:41 -0600

    First Commit

---
+++ src/starter/chess/CBoard.java
@@ -0,0 +1,19 @@
+package chess;
+
+public class CBoard implements ChessBoard{
+
+    @Override
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+
+    }
+
+    @Override
+    public ChessPiece getPiece(ChessPosition position) {
+        return null;
+    }
+
+    @Override
+    public void resetBoard() {
+
+    }
+}
