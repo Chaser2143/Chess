a2587d3de3218da6d9da423ca1c5b444749aa21b
Chase Bledsoe <cb986@byu.edu>
2023-10-14 19:38:14 -0600

    Model Classes Implemented

--- src/main/models/Game.java
+++ src/main/models/Game.java
@@ -1,4 +1,59 @@
 package models;
 
+import chess.CGame;
+
 public class Game {
+    private int gameID;
+    private String whiteUsername;
+    private String blackUsername;
+    private String gameName;
+    private CGame game;
+
+    public Game(int gameID, String whiteUsername, String blackUsername, String gameName, CGame game) {
+        this.gameID = gameID;
+        this.whiteUsername = whiteUsername;
+        this.blackUsername = blackUsername;
+        this.gameName = gameName;
+        this.game = game;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public void setWhiteUsername(String whiteUsername) {
+        this.whiteUsername = whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
+
+    public void setBlackUsername(String blackUsername) {
+        this.blackUsername = blackUsername;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public CGame getGame() {
+        return game;
+    }
+
+    public void setGame(CGame game) {
+        this.game = game;
+    }
 }
