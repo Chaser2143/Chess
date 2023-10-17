d1b1829597e90d43977424c0b0fd65bc27d93d9a
Chase Bledsoe <cb986@byu.edu>
2023-10-17 10:45:32 -0600

    Added Comments to LoginResponse

--- src/main/reqRes/LoginRes.java
+++ src/main/reqRes/LoginRes.java
@@ -1,36 +1,69 @@
 package reqRes;
 
-public class LoginRes {
+/**
+ * Serves as the response for the login API
+ */
+public class LoginRes extends Response{
     private String message;
     private String authToken;
     private String username;
 
+    /**
+     * Constructor for Login Response
+     * @param message Message for response
+     * @param authToken Authtoken generated
+     * @param username Username Associated
+     */
     public LoginRes(String message, String authToken, String username) {
-        this.message = message;
+        super(message);
         this.authToken = authToken;
         this.username = username;
     }
 
+    /**
+     * Gets the Response Message
+     * @return string
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the Response message
+     * @param message becomes the new message
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Returns the response auth token
+     * @return String
+     */
     public String getAuthToken() {
         return authToken;
     }
 
+    /**
+     * Sets the Response Auth Token
+     * @param authToken becomes the new auth token
+     */
     public void setAuthToken(String authToken) {
         this.authToken = authToken;
     }
 
+    /**
+     * Gets the Response Username
+     * @return string
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the response username
+     * @param username becomes the new username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
